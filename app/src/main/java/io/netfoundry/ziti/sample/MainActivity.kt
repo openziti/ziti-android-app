0c90568c9014e44a8680b6dbe72b5ac33012eac3
Eugene K <eugene.kobyakov@netfoundry.io>
2020-03-05 13:52:44 -0500

    use ziti-android library, move unnecessary cruft

--- app/src/main/java/io/netfoundry/ziti/sample/MainActivity.kt
+++ app/src/main/java/io/netfoundry/ziti/sample/MainActivity.kt
@@ -1,40 +1,56 @@
 package io.netfoundry.ziti.sample
 
-import android.app.Activity
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.os.Handler
-import android.provider.Settings
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
-import android.util.Log
 import android.view.Menu
-import android.view.MenuItem
-import android.widget.Toast
-import android.widget.Toast.LENGTH_LONG
-import io.netfoundry.ziti.Ziti
+import io.netfoundry.ziti.android.Ziti
 import kotlinx.android.synthetic.main.activity_main.*
-import java.security.KeyStore
+import kotlinx.android.synthetic.main.content_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import java.net.HttpURLConnection
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
-    val SELECT_ENROLLMENT_JWT = 2171
-    lateinit var ks: KeyStore
+    val url = "http://weather.ziti.netfoundry.io/Charlotte?format=3"
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        ks = KeyStore.getInstance("AndroidKeyStore")
-        ks.load(null)
-
-        Ziti.init(ks, true)
+        Ziti.init(applicationContext)
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+            loadData()
+        }
+    }
+
+    fun loadData() = GlobalScope.launch(Dispatchers.IO) {
+        val body = async {
+            val con = URL(url).openConnection() as HttpURLConnection
+            con.addRequestProperty("Host", "wttr.in")
+            if (con.responseCode > 200) {
+                throw Exception(con.responseMessage)
+            } else {
+                con.inputStream.reader().readText()
+            }
+        }
+
+        body.invokeOnCompletion { ex ->
+            if (ex != null) {
+                Snackbar.make(fab, ex.localizedMessage, Snackbar.LENGTH_LONG)
+                    .setAction("Dismiss", null).show()
+            }
+        }
+
+        val text = body.await()
+        result_text.post {
+            result_text.text = text
         }
     }
 
@@ -42,59 +58,5 @@ class MainActivity : AppCompatActivity() {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.menu_main, menu)
         return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            R.id.enroll_ziti -> {
-                val intent = Intent(Intent.ACTION_GET_CONTENT).apply {
-                    type ="*/*"
-                }
-                startActivityForResult(intent, SELECT_ENROLLMENT_JWT)
-                true
-            }
-
-            else -> super.onOptionsItemSelected(item)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        when(requestCode) {
-            SELECT_ENROLLMENT_JWT -> {
-                if (resultCode == Activity.RESULT_OK) {
-                    data?.data?.let {
-                        enrollZiti(it)
-                    }
-                }
-            }
-            else -> super.onActivityResult(requestCode, resultCode, data)
-        }
-    }
-
-    fun enrollZiti(jwtUri: Uri) {
-
-        val jwt = contentResolver.openInputStream(jwtUri)!!.readBytes()
-        val name = Settings.Global.getString(contentResolver, "device_name")
-
-        Thread {
-            try {
-                Ziti.enroll(ks, jwt, name)
-                showResult("Enrollment Success!!")
-            } catch (ex: Exception) {
-                Log.w("sample", "exeption", ex)
-                showResult(ex.localizedMessage)
-            }
-        }.start()
-    }
-
-    fun showResult(str: String) {
-        Handler(mainLooper).post {
-            Toast.makeText(this, str, LENGTH_LONG).show()
-
-        }
     }
 }
